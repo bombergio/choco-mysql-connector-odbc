e11d7c87006280616137855a5beaf85fdf4adb60
Bombergio <cosmaori@gmail.com>
2019-03-15 16:07:38 +0100

    First commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,44 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://dev.mysql.com/get/Downloads/Connector-ODBC/5.3/mysql-connector-odbc-5.3.12-win32.msi'
+$url64      = 'https://dev.mysql.com/get/Downloads/Connector-ODBC/5.3/mysql-connector-odbc-5.3.12-winx64.msi'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'msi'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'mysql odbc driver *'
+
+  checksum      = '4cd52796decad55c038c62b687735b1d'
+  checksumType  = 'md5'
+  checksum64    = 'a5b8486f5e573863937e70f48871aef2'
+  checksumType64= 'md5'
+
+  silentArgs    = "/qn"
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
