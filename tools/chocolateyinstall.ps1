95956bffa893b2add16ab35d5ff156f0b36e1f2b
Daniils Kostornijs <daniils.kostornijs@intenthq.com>
2021-10-16 15:08:52 +0200

    A new start

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,24 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://cdn.mysql.com//Downloads/Connector-ODBC/8.0/mysql-connector-odbc-8.0.26-win32.msi'
+$url64      = 'https://cdn.mysql.com//Downloads/Connector-ODBC/8.0/mysql-connector-odbc-8.0.26-winx64.msi'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'msi'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'mysql odbc driver *'
+
+  checksum      = '8f7807f903bda2f65f169f7935815ef9b510c70cf19cfb64ca622037a6ac8613'
+  checksumType  = 'sha256'
+  checksum64    = 'accb448d8a27974c764a89727dd7215b59523361ba2131088cd5267c9d551a01'
+  checksumType64= 'sha256'
+
+  silentArgs    = "/qn"
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
