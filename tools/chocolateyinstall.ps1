043204892398fecd8597744b58e6d4736291ba49
bombergio <6366405+bombergio@users.noreply.github.com>
2023-01-18 10:50:25 +0100

    Updating MySQL ODBC driver to 8.0.32 (#18)
    Co-authored-by: bombergio <bombergio@users.noreply.github.com>

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://cdn.mysql.com//Downloads/Connector-ODBC/8.0/mysql-connector-odbc-8.0.31-win32.msi'
-$url64      = 'https://cdn.mysql.com//Downloads/Connector-ODBC/8.0/mysql-connector-odbc-8.0.31-winx64.msi'
+$url        = 'https://cdn.mysql.com//Downloads/Connector-ODBC/8.0/mysql-connector-odbc-8.0.32-win32.msi'
+$url64      = 'https://cdn.mysql.com//Downloads/Connector-ODBC/8.0/mysql-connector-odbc-8.0.32-winx64.msi'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -12,9 +12,9 @@ $packageArgs = @{
 
   softwareName  = 'mysql odbc driver *'
 
-  checksum      = '2b5e19122823c835e7a2a9e7aaafc7720ba5cd67517a19b623bd756b862d5060'
+  checksum      = '51edbc90abdecc8543aa6462486d51880886fafba98ba9d06ea058d8e8a5be88'
   checksumType  = 'sha256'
-  checksum64    = 'e75f2a9b0b31abe7c803f5aaa9031d838e55652a12c12550ed45758d844cdd48'
+  checksum64    = 'f10581c0feec2159fe8158eab4580f77c783d86aeaf4b6071a7e6bc971a7927e'
   checksumType64= 'sha256'
 
   silentArgs    = "/qn"
