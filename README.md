d41660dfedde47f81d4c36b226255f865617618e
Bombergio <=>
2023-02-18 16:30:10 +0100

    Cleanup and readme

---
+++ README.md
@@ -0,0 +1,26 @@
+# MySQL Connector/ODBC Driver Chocolatey package
+
+## Description
+This is a definition for the MySQL Connector/ODBC Driver Chocolatey package.
+
+The package can be found here: https://community.chocolatey.org/packages/mysql.odbc
+
+## Deployment Method: Individual Install, Upgrade, & Uninstall
+
+### Install
+To install MySQL Connector/ODBC Driver (Install), run the following command from the command line or from PowerShell:
+```
+choco install mysql.odbc
+```
+
+### Upgrade
+To upgrade MySQL Connector/ODBC Driver (Install), run the following command from the command line or from PowerShell:
+```
+choco upgrade mysql.odbc
+```
+
+### Uninstall
+To uninstall MySQL Connector/ODBC Driver (Install), run the following command from the command line or from PowerShell:
+```
+choco uninstall mysql.odbc
+```
--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,24 +1,24 @@
 ﻿$ErrorActionPreference = 'Stop';
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://cdn.mysql.com//Downloads/Connector-ODBC/8.0/mysql-connector-odbc-8.0.32-win32.msi'
-$url64      = 'https://cdn.mysql.com//Downloads/Connector-ODBC/8.0/mysql-connector-odbc-8.0.32-winx64.msi'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://cdn.mysql.com//Downloads/Connector-ODBC/8.0/mysql-connector-odbc-8.0.32-win32.msi'
+$url64 = 'https://cdn.mysql.com//Downloads/Connector-ODBC/8.0/mysql-connector-odbc-8.0.32-winx64.msi'
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
+  packageName = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  fileType      = 'msi'
-  url           = $url
-  url64bit      = $url64
+  fileType = 'msi'
+  url = $url
+  url64bit = $url64
 
-  softwareName  = 'mysql odbc driver *'
+  softwareName = 'mysql odbc driver *'
 
-  checksum      = '51edbc90abdecc8543aa6462486d51880886fafba98ba9d06ea058d8e8a5be88'
-  checksumType  = 'sha256'
-  checksum64    = 'f10581c0feec2159fe8158eab4580f77c783d86aeaf4b6071a7e6bc971a7927e'
-  checksumType64= 'sha256'
+  checksum = '51edbc90abdecc8543aa6462486d51880886fafba98ba9d06ea058d8e8a5be88'
+  checksumType = 'sha256'
+  checksum64 = 'f10581c0feec2159fe8158eab4580f77c783d86aeaf4b6071a7e6bc971a7927e'
+  checksumType64 = 'sha256'
 
-  silentArgs    = "/qn"
-  validExitCodes= @(0)
+  silentArgs = "/qn"
+  validExitCodes = @(0)
 }
 
 Install-ChocolateyPackage @packageArgs
