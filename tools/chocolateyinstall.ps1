23bfe08d56e0097cad4f5ac73dcfa1c4be0e0bab
bombergio <bombergio@users.noreply.github.com>
2024-01-16 08:06:10 +0000

    Updating MySQL ODBC driver to 8.3.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64 = 'https://cdn.mysql.com//Downloads/Connector-ODBC/8.2/mysql-connector-odbc-8.2.0-winx64.msi'
+$url64 = 'https://cdn.mysql.com//Downloads/Connector-ODBC/8.3/mysql-connector-odbc-8.3.0-winx64.msi'
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
@@ -10,7 +10,7 @@ $packageArgs = @{
 
   softwareName = 'mysql odbc driver *'
 
-  checksum64 = '182e32303c7da2ef9c01b05a287f23d0185ca682fe0a616f69f65cbafa5cd708'
+  checksum64 = 'ffecc1317d39a546ae73f7ac2fa5031b6dc5b6ab41e0eb0af3089f2e05e98fea'
   checksumType64 = 'sha256'
 
   silentArgs = "/qn"
