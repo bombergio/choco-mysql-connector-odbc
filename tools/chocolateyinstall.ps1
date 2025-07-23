24e7c968cedc26af1b634837550f8a17a176811a
bombergio <bombergio@users.noreply.github.com>
2025-07-23 08:09:42 +0000

    Updating MySQL ODBC driver to 9.4.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64 = 'https://cdn.mysql.com//Downloads/Connector-ODBC/9.3/mysql-connector-odbc-9.3.0-winx64.msi'
+$url64 = 'https://cdn.mysql.com//Downloads/Connector-ODBC/9.4/mysql-connector-odbc-9.4.0-winx64.msi'
 
 $packageArgs = @{
   packageName = $env:ChocolateyPackageName
@@ -10,7 +10,7 @@ $packageArgs = @{
 
   softwareName = 'mysql odbc driver *'
 
-  checksum64 = '0a5520204edc17c877b744ce3f804e95d85333c1ee7c23a77cbb4105b5c616a2'
+  checksum64 = '9e0679eb370b777545f10b931a716011d70fa810913a2b4ec14d60abaf59c610'
   checksumType64 = 'sha256'
 
   silentArgs = "/qn"
